01560b2613b54fa044ca9633c8dc0dc7f6bf3dac
eug.raczynski@gmail.com <eug.raczynski@gmail.com>
2023-12-12 21:41:22 +0100

    adding header

--- pageObjects/pages/homePage.ts
+++ pageObjects/pages/homePage.ts
@@ -3,16 +3,12 @@ import { expect, type Locator, type Page} from '@playwright/test'
 export class BasePage {
     readonly page: Page
     readonly bigPage: Locator
-    readonly landingPage: Locator
-    readonly pricingPage: Locator
-    readonly formPage: Locator
-    readonly dynamicPage: Locator
-    readonly loginPage: Locator
-    readonly interactionPage: Locator
+    readonly header: Locator
 
     constructor(page: Page) {
         this.page = page
         this.bigPage = page.locator('a', { hasText: 'Big page with many elements' })
+        this.header = page.locator('#main-menu')
     }
 
     async gotoStart() {
@@ -22,4 +18,9 @@ export class BasePage {
     async openBigPage() {
         await this.bigPage.click()
     }
+
+    async openBlog() {
+        await this.header.locator('a', { hasText: 'Blog'}).click()
+    }
 }
+
--- tests/openBigPage.spec.ts
+++ tests/openBigPage.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test'
-import { BasePage } from '../pageObjects/pages/basePage'
+import { BasePage } from '../pageObjects/pages/homePage'
 
 test('Goto complicated page',async ({ page }) => {
     const startingPage = new BasePage(page);
-    await startingPage.gotoStart();
+    await startingPage.gotoStart()
     await startingPage.openBigPage()
     await expect(page).toHaveURL('https://ultimateqa.com/complicated-page')
+    await startingPage.openBlog()
+    await expect(page).toHaveURL('https://ultimateqa.com/blog/')
 })
